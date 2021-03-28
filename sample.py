4e2beeffcd77ed4be618fbb21a63eed9e202be10
Minyus <yusuke.minami86@gmail.com>
2021-03-29 00:35:53 +0800

    Avoid re-applying SmallDict

--- sample.py
+++ sample.py
@@ -3,9 +3,15 @@ from smalldict import SmallDict
 
 
 for p in Path(".").rglob("*.json"):
-    print(p)
-    SmallDict(str(p)).get(
-        list_limit=10,
-        json_out=str(p) + "_samples.json",
-        yaml_out=str(p) + "_samples.yaml",
-    )
+    if "_samples" not in str(p):
+        print("[Processing] {}".format(p))
+        try:
+            SmallDict(str(p)).get(
+                list_limit=10,
+                json_out=str(p) + "_samples.json",
+                yaml_out=str(p) + "_samples.yaml",
+            )
+        except Exception:
+            import traceback
+
+            print(traceback.format_exc())
